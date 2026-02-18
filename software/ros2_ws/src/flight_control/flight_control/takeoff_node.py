2e42338548d7c7e63c4ff7533d90c3c0fbe6ce4c
Reja Haddad <rejahaddad@Mac.olin.edu>
2026-02-17 21:21:46 -0500

    fixed nodes

--- software/ros2_ws/src/flight_control/flight_control/takeoff_node.py
+++ software/ros2_ws/src/flight_control/flight_control/takeoff_node.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
+#IDK IF THE SHEBANG IS RIGHT
 import rclpy
 from rclpy.node import Node
-from mavros_msgs.srv import CommandBool #ADD DEPENDANCY
+from mavros_msgs.srv import CommandTOL
 #if you import more libraries, add them to dependencies in package.xml
 
-class arm_node(Node):
+class takeoff_class(Node):
+
     def __init__(self):
-        super().__init__('armNode')
-        self.get_logger().info('arming node has been started')
-        self.arming_client = self.create_client(CommandBool, '/mavros/cmd/arming')
-        while not self.arming_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for /mavros/cmd/arming service...')
-        self.req = CommandBool.Request()
-    def send_request(self, msg):
-        self.req.value = msg#MAKE SURE ITS CALLED VALUE
-        self.future = self.arming_client.call_async(self.req)
+        super().__init__('takeoff_node')
+        self.get_logger().info('takeoff node has been started')
+        self.cli = self.create_client(CommandTOL, 'mavros/cmd/takeoff')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = CommandTOL.Request()
+
+    def send_request(self, altitude, lat, lon, min_pitch, yaw):
+        self.req.altitude = altitude
+        self.req.latitude = lat
+        self.req.longitude = lon
+        self.req.min_pitch = min_pitch
+        self.req.yaw = yaw
+        self.future = self.cli.call_async(self.req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
 
-        # Add your node logic here
-        # For example, you can create a timer, subscribe to topics, etc.
-
-
 def main(args=None):
     rclpy.init(args=args)
-    node = arm_node()
-    response = node.send_request(True)#MIGHT WANT TO MAKE OPPOSITE OF WHAT IT IS not true
-    arm_node.get_logger().info("Status of arming:", response.success)#IDK IF ITS SUCCESS
-    arm_node.destroy_node()
-    #rclpy.spin(node) #Loops the node
+    takeoff_node = takeoff_class()
+    altitude, lat, lon, min_pitch, yaw = 2.0,0.0,0.0,0.0,0.0
+    response = takeoff_node.send_request(altitude, lat, lon, min_pitch, yaw)
+    takeoff_node.get_logger().info(f"results: {response.success} bool and uint f{response.result}")
+    takeoff_node.destroy_node()
     rclpy.shutdown()
 
 
-if __name__=='__main__':#Optional if you only ever run the node directly with ros2 run 
+if __name__ == '__main__':
     main()
